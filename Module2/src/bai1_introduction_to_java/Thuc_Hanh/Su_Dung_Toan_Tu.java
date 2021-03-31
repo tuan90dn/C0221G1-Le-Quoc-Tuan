28d9f2c17cf6a7d76a3278f520c6627565c160dd
tuan90dn <tuanql90@gmail.com>
2021-03-31 14:16:37 +0700

    update

---
+++ Module2/src/bai1_introduction_to_java/Thuc_Hanh/Su_Dung_Toan_Tu.java
@@ -0,0 +1,4 @@
+package bai1_introduction_to_java.Thuc_Hanh;
+
+public class Su_Dung_Toan_Tu {
+}
--- Module2/src/bai1_introduction_to_java/Thuc_Hanh/SystemTime.java
+++ Module2/src/bai1_introduction_to_java/Thuc_Hanh/SystemTime.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+
 public class SystemTime {
     public static void main(String[] args) {
         Date now=new Date();
