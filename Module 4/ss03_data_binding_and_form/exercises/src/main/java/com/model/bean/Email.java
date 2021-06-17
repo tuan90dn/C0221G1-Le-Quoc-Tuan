60a5984eedfe233461f87a64b8b3e59b14c2c7cb
tuan90dn <tuanql90@gmail.com>
2021-06-17 10:24:55 +0700

    update

---
+++ Module 4/ss03_data_binding_and_form/exercises/src/main/java/com/model/bean/Email.java
@@ -0,0 +1,50 @@
+package com.model.bean;
+
+public class Email {
+    private String language;
+    private int pageSize;
+    private boolean spamsFilter;
+    private String signature;
+
+    public Email(){
+    }
+
+    public Email(String language, int pageSize, boolean spamsFilter, String signature) {
+        this.language = language;
+        this.pageSize = pageSize;
+        this.spamsFilter = spamsFilter;
+        this.signature = signature;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public boolean getSpamsFilter() {
+        return spamsFilter;
+    }
+
+    public void setSpamsFilter(boolean spamsFilter) {
+        this.spamsFilter = spamsFilter;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+}
