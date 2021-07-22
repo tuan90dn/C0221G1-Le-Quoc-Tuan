02abb0a69c34246e578e5fde4e2cbc14ac63124a
tuan90dn <tuanql90@gmail.com>
2021-07-22 21:29:30 +0700

    update

--- Module_5/ss03_angular_overview/exercises/angular-exercises/src/app/color-picker/color-picker.component.ts
+++ Module_5/ss03_angular_overview/exercises/angular-exercises/src/app/color-picker/color-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-color-picker',
@@ -6,14 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color-picker.component.scss']
 })
 export class ColorPickerComponent implements OnInit {
-  horizontal = '5';
-  vertical = '5';
-  blur = '5';
-  spread = '5';
-  color = 'red';
+  @Input() heading: string;
+  @Input() color: string;
+  @Output() event: EventEmitter<string> = new EventEmitter<string>();
+
+  public show = false;
+  public defaultColors: string[] = [
+    '#ffffff',
+    '#000105',
+    '#3e6158',
+    '#3f7a89',
+    '#96c582',
+    '#b7d5c4',
+    '#bcd6e7',
+    '#7c90c1',
+    '#9d8594',
+    '#dad0d8',
+    '#4b4fce',
+    '#4e0a77',
+    '#a367b5',
+    '#ee3e6d',
+    '#d63d62',
+    '#c6a670',
+    '#f46600',
+    '#cf0500',
+    '#efabbd',
+    '#8e0622',
+    '#f0b89a',
+    '#f0ca68',
+    '#62382f',
+    '#c97545',
+    '#c1800b'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  public changeColor(color: string): void {
+  this.color = color;
+  this.event.emit(this.color);
+  this.show = false;
+  }
+
+
+
+  public changeColorManual(color: string): void {
+  const isValid = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color);
+
+  if (isValid) {
+  this.color = color;
+  this.event.emit(this.color);
+  }
+  }
+
+
+  public toggleColors(): void {
+  this.show = !this.show;
+  }
 }
